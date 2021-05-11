90481f07783f4faa915065ac3dd0db197e7186a6
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-11 17:14:43 -0500

    node-db2 created database and cars-model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,16 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db("cars");
+};
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+  return db("cars").where({ id }).first();
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const create = async (car) => {
+  const [id] = await db("car").insert(car);
+  return getById(id);
+};
+
+exports.module = { getAll, getById, create };
